1ce05dab68e89bf4a7ae7415369fc5f5f04ab09e
teiderrems <raoul.teida@facsciences-uy1.cm>
2026-02-09 14:59:15 +0100

    feat: integrate Firebase emulators and update app configuration
    - Added Firebase emulator configurations for Auth, Firestore, and Storage in app.config.ts.
    - Introduced a new environment file to manage emulator settings.
    - Updated package.json to include Firebase dependencies and new scripts for starting emulators.
    - Removed outdated app.spec.ts file.
    - Enhanced app.ts to inject Firebase services and log emulator configurations.
    - Added a cache file for Firebase hosting.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,41 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth, connectAuthEmulator } from '@angular/fire/auth';
+import { provideFirestore, getFirestore, connectFirestoreEmulator } from '@angular/fire/firestore';
+import { provideStorage, getStorage, connectStorageEmulator } from '@angular/fire/storage';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideFirebaseApp(() =>
+      initializeApp({
+        apiKey: 'fake',
+        authDomain: 'localhost',
+        projectId: 'todolist-ghizlane',
+        storageBucket: "gs://todolist-with-em.firebasestorage.app/",
+      })
+    ),
+
+    provideAuth(() => {
+      const auth = getAuth();
+      connectAuthEmulator(auth, 'http://localhost:8081');
+      return auth;
+    }),
+
+    provideFirestore(() => {
+      const firestore = getFirestore();
+      connectFirestoreEmulator(firestore, 'localhost', 8082);
+      return firestore;
+    }),
+    provideStorage(() => {
+      const storage = getStorage();
+      connectStorageEmulator(storage, 'localhost', 8084);
+      return storage;
+    })
   ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,21 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { Auth } from '@angular/fire/auth';
+import { Firestore } from '@angular/fire/firestore';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
   protected readonly title = signal('todo_list');
+  private readonly auth = inject(Auth);
+  private readonly firestore = inject(Firestore);
+
+  constructor() {
+      console.log('Auth Emulator Config:', this.auth);
+      console.log('firestore Emulator Config:', this.firestore);
+  }
 }
